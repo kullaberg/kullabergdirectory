1f68c1f907f1a1d12f7defbfdc58a113263da808
Carlos Velasco <carlos@2activedesign.com>
2017-09-22 16:04:49 +0200

    categories added

--- js/master.js
+++ js/master.js
@@ -1,8 +1,14 @@
-let Businesses = new Set();
+const listings = document.getElementById("listings");
+window.All = new Set();
+window.Guide = new Set();
+window.Dining = new Set();
+window.Cafe = new Set();
+window.Hotel = new Set();
+window.Grocery = new Set();
 class Business {
   /**
    * Creates an instance of Business.
-   * @param {string} businessName 
+   * @param {string} name 
    * @param {string} email 
    * @param {string} phone 
    * @param {string} address 
@@ -10,94 +16,210 @@ class Business {
    * @param {string} location 
    * @memberof Business
    */
-  constructor(businessName, email, phone, address, description, location) {
-    this.businessName = businessName;
-    this.email = email;
-    this.phone = phone;
-    this.address = address;
-    this.description = description;
-    this.location = location;
-    Businesses.add(this);
+  constructor({
+    name,
+    email,
+    phone,
+    website,
+    address,
+    description,
+    location,
+    category
+  }) {
+    this.name = name || "Not Available";
+    this.email = email || "Not Available";
+    this.phone = phone || "Not Available";
+    this.website = website || "Not Available";
+    this.address = address || "Not Available";
+    this.description = description || "Not Available";
+    this.location = location || "Not Available";
+    this.locationLink = location || "Not Available";
+    this.category = category || "Not Available";
+    switch (category) {
+      case "Dining":
+        Dining.add(this);
+        break;
+      case "Cafe":
+        Cafe.add(this);
+        break;
+      case "Guide":
+        Guide.add(this);
+        break;
+      case "Hotel":
+        Hotel.add(this);
+        break;
+      case "Grocery":
+        Grocery.add(this);
+        break;
+
+      default:
+        break;
+    }
+    All.add(this);
+    window[name] = this;
   }
 
   get card() {
-    return `<div class="col m6 l4">
-    <article class="card horizontal blue darken-3 hide-on-small-only">
+    return `<li><article class="col m6 l4">
+    <div class="card horizontal blue darken-3 hide-on-small-only">
   <div class="card-stacked">
     <div class="card-content grey-text text-lighten-4">
-      <span class="card-businessName blue-text text-lighten-3">${this
-        .businessName}</span>
-      <ul>
-        <li>${this.email}</li>
-        <li>${this.phone}</li>
-        <li>${this.address}</li>
-      </ul>
-      <p>
-      ${this.description}
-      </p>
+      <h5 class="card-name blue-text text-lighten-4">${this.name}</h5>
+        <h6>${this.address}</h6>
+        <p>${this.description}</p>
     </div>
     <div class="card-action">
-      <h6 class="blue-text text-lighten-3">Location: ${this.location}</h6>
+    <a href="${this
+      .phone}" class="blue-text text-lighten-3"><i class="material-icons">phone</i></a>
+    <a href="${this
+      .email}" class="blue-text text-lighten-3"><i class="material-icons">email</i></a>
+    <a href="${this
+      .locationLink}" class="blue-text text-lighten-3"><i class="material-icons">place</i></a>
+    <a href="${this
+      .website}" class="blue-text text-lighten-3"><i class="material-icons">open_in_browser</i></a>
     </div>
   </div>
-</article>
 </div>
+</article></li>
+<li>
 <article class="card blue hide-on-med-and-up">
   <div class="card blue darken-3">
     <div class="card-content grey-text text-lighten-4">
-      <span class="card-businessName blue-text text-lighten-3">${this
-        .businessName}</span>
-      <ul>
-        <li>${this.email}</li>
-        <li>${this.phone}</li>
-        <li>${this.address}</li>
-      </ul>
+      <span class="card-name blue-text text-lighten-4">${this.name}</span>
+        <div class="row">
+        <div class="col s4">Email:</div><div class="col s8">${this.email}</div>
+        <div class="col s4">Phone:</div><div class="col s8">${this.phone}</div>
+        <div class="col s4"> Address:</div><div class="col s8">${this
+          .address}</div>
+      </div>
       <p>
       ${this.description}
       </p>
     </div>
     <div class="card-action">
-      <h6 class="blue-text text-lighten-3">Location: ${this.location}</h6>
+      <a href="${this
+        .locationLink} class="blue-text text-lighten-3">Location</a>
+      <a href="${this.website}" class="blue-text text-lighten-3">Website</a>
     </div>
   </div>
-</article>`;
+</article></li>`;
   }
 }
 
-const cafe1 = new Business(
-  "Cafe1",
-  "cafe@test.com",
-  "223-2144",
-  "Hoganas, SV",
-  "We sell coffee and tea",
-  "coordintaes here"
-);
-const cafe2 = new Business(
-  "Cafe1",
-  "cafe@test.com",
-  "223-2144",
-  "Hoganas, SV",
-  "We sell coffee and tea",
-  "coordintaes here"
-);
-const cafe3 = new Business(
-  "Cafe1",
-  "cafe@test.com",
-  "223-2144",
-  "Hoganas, SV",
-  "We sell coffee and tea",
-  "coordintes here"
-);
-const cafe55 = new Business(
-  "Cafe1",
-  "cafe@test.com",
-  "223-2144",
-  "Hoganas, SV",
-  "We sell coffee and tea",
-  "coordinates here"
-);
-const movieSection = document.getElementById("movieCard");
+new Business({
+  name: "CafeTesting",
+  email: "cafe@test.com",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Grocery",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Restaurant",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Guide",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Cafe",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Hotel",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Hotel",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Dining",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Dining",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Guide",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
+new Business({
+  name: "Cafe1",
+  email: "cafe@test.com",
+  phone: "223-2144",
+  website: "URL goes here",
+  address: "Hoganas, SV",
+  category: "Dining",
+  description: "We sell coffee and tea",
+  location: "coordintaes here"
+});
 
-Businesses.forEach(function(element) {
-  movieSection.innerHTML += element.card;
-}, this);
+window.addEventListener("load", function() {
+  All.forEach(function(element) {
+    listings.innerHTML += element.card;
+  }, this);
+  Materialize.showStaggeredList("#listings");
+});
+
+window.sort = function(category) {
+  $("#listings").fadeOut(300);
+  $("#listings").fadeIn(0);
+  listings.innerHTML = "";
+  window[category].forEach(function(element) {
+    console.log(category);
+    listings.innerHTML += element.card;
+  }, this);
+  Materialize.showStaggeredList("#listings");
+};
